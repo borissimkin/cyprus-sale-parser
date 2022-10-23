f60e4b66fd1dbd8d7b086e5d3891c0bd9b0816e3
boris <simkin.boris.b@gmail.com>
2022-10-23 14:10:15 +0300

    fix

--- src/bot/wordParser/isSomeNotImportantMessage.ts
+++ src/bot/wordParser/isSomeNotImportantMessage.ts
@@ -4,11 +4,11 @@ const infoMessages: string[] = [
     "ознакомьтесь с правилами во избежание ограничений",
     "Подобные ФОРУМЫ / чаты у нас по",
     "нужно указывать цену,город, краткое описание. Если отдаете, указывайте, что"
-]
+].map((info) => info.toLowerCase())
 
 export const isSomeNotImportantMessage = (text: string) => {
     const textLowerCase = text.toLowerCase()
     return infoMessages.some((infoMessage) => {
-        return textLowerCase.includes(infoMessage.toLowerCase())
+        return textLowerCase.includes(infoMessage)
     })
 }
